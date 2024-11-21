a1a1f57207a93213a86a15649f3c52eab0cf080c
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-21 20:28:57 +0700

    NavHost bertanggung jawab untuk mengelola navigasi antar halaman

--- app/src/main/java/com/example/project5/PengelolaHalaman.kt
+++ app/src/main/java/com/example/project5/PengelolaHalaman.kt
@@ -1,11 +1,14 @@
 package com.example.project5
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.project5.ui.viewmodel.MahasiswaViewModel
 
@@ -22,7 +25,9 @@ fun PengelolaHalaman(
 ){
     Scaffold { isipadding ->
         val uiState by viewModel.uiState.collectAsState()
-
+        NavHost(
+            modifier = modifier.padding(isipadding),
+            navController = navHost, startDestination = Halaman.Form.name) { }
 
     }
 
