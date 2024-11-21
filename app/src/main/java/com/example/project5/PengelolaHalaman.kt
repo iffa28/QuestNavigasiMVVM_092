ba89fc87b393b20446b58c2c145579a1b67940d5
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-21 19:23:02 +0700

    menambahkan fungsi dan parameter fungsi

--- app/src/main/java/com/example/project5/PengelolaHalaman.kt
+++ app/src/main/java/com/example/project5/PengelolaHalaman.kt
@@ -1,6 +1,23 @@
 package com.example.project5
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.project5.ui.viewmodel.MahasiswaViewModel
+
 enum class Halaman{
     Form,
     Data
+}
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+
+){
+
 }
